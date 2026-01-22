164d0ee056b624cb161cc041162481351e3f5f81
fukumen <fukumen@gmail.com>
2026-01-22 15:57:40 +0900

    警告が出ないように修正

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php
-error_reporting(E_ALL & ~(E_NOTICE | E_STRICT | E_DEPRECATED));
+error_reporting(E_ALL & ~(E_NOTICE | E_DEPRECATED));
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/conf/conf.inc.php';
