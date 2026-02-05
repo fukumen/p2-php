7c073fc18975240e772296162db26444cd201d9b
Fukumen <fukumen@gmail.com>
2026-02-05 15:52:29 +0900

    vscode用の修正

---
+++ _ide_helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * IDE Helper for rep2
+ */
+
+/** @var Login $_login */
+$_login = new Login();
+
+/** @var array $_conf */
+$_conf = array();
+
+/** @var Session $_p2session */
+$_p2session = new Session();
