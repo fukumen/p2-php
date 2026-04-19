0e6794a46ed2ed8f218774400840a32fd43bad2f
Fukumen <fukumen@gmail.com>
2026-04-19 12:39:18 +0900

    ハイライトのスマホの夜の対応
    * CSSで上書きするためのclassを追加
    
    かなり変な感じになってしまったが、他にできそうなのはスマホの$STYLEは空文字にしておいて昼もCSSにするぐらいだろうか。
    本当はspanでclassだけ指定するようにしてPCも含めて修正するのがよいかも。

--- lib/live/live_highlight_convert.php
+++ lib/live/live_highlight_convert.php
@@ -10,7 +10,7 @@ $live_highlight_chain_style = "background-color: {$STYLE['live_highlight_chain']
 if ($ng_type & self::HIGHLIGHT_CHAIN) {
 	$pattern_chain_nums = implode('|', array_intersect($highlight_chain_nums, $this->_highlight_nums));
 	$pattern_chain_nums = "(" . $pattern_chain_nums . ")(?!\d)(?![^<]*>)"; // 数字が一部被ってしまうアンカーとHTMLタグ内にマッチさせない
-	$msg = preg_replace("(((?:&gt;|＞|-)+)($pattern_chain_nums))", "<span style=\"{$live_highlight_chain_style}\">$1$2</span>", $msg);
+	$msg = preg_replace("(((?:&gt;|＞|-)+)($pattern_chain_nums))", "<span class=\"live_highlight_chain\" style=\"{$live_highlight_chain_style}\">$1$2</span>", $msg);
 }
 
 // ハイライトメッセージ変換
@@ -30,9 +30,9 @@ if ($ng_type & self::HIGHLIGHT_MSG) {
 			$pattern = "(" . $word . ")(?![^<]*>)"; // HTMLタグ内にマッチさせない
 
 			if ($ignore_case) {
-				$msg = mb_eregi_replace($pattern, "<span style=\"{$live_highlight_style}\">\\1</span>", $msg);
+				$msg = mb_eregi_replace($pattern, "<span class=\"live_highlight\" style=\"{$live_highlight_style}\">\\1</span>", $msg);
 			} else {
-				$msg = mb_ereg_replace($pattern, "<span style=\"{$live_highlight_style}\">\\1</span>", $msg);
+				$msg = mb_ereg_replace($pattern, "<span class=\"live_highlight\" style=\"{$live_highlight_style}\">\\1</span>", $msg);
 			}
 		}
 	}
@@ -41,25 +41,25 @@ if ($ng_type & self::HIGHLIGHT_MSG) {
 // ハイライトネーム変換
 if ($ng_type & self::HIGHLIGHT_NAME) {
 	$name = preg_replace("(<b>|</b>)", "", $name);
-	$name = "</b><span style=\"{$live_highlight_style}\">$name</span><b>";
+	$name = "</b><span class=\"live_highlight\" style=\"{$live_highlight_style}\">$name</span><b>";
 } elseif ($ng_type & self::HIGHLIGHT_WATCHOI) {
 	if ($_conf['ngaborn_watchoi4']) {
 		$pattern = '#\(([^\s()]+\s+)?([0-9A-Za-z./*+]{4}-)([0-9A-Za-z./*+]{4}(?:\s+\[.+])?)\)#';
-		$name = preg_replace($pattern, "($1<span style=\"{$live_highlight_style}\">$2</span>$3)", $name);
+		$name = preg_replace($pattern, "($1<span class=\"live_highlight\" style=\"{$live_highlight_style}\">$2</span>$3)", $name);
 	} else {
 		$pattern = '#\(((?:[^\s()]+\s+)?(?:[0-9A-Za-z./*+]{4}-[0-9A-Za-z./*+]{4})(?:\s+\[.+])?)\)#';
-		$name = preg_replace($pattern, "(<span style=\"{$live_highlight_style}\">$1</span>)", $name);
+		$name = preg_replace($pattern, "(<span class=\"live_highlight\" style=\"{$live_highlight_style}\">$1</span>)", $name);
 	}
 }
 
 // ハイライトメール変換
 if ($ng_type & self::HIGHLIGHT_MAIL) {
-	$mail = "<span style=\"{$live_highlight_style}\">$mail</span>";
+	$mail = "<span class=\"live_highlight\" style=\"{$live_highlight_style}\">$mail</span>";
 }
 
 // ハイライトID変換
 if ($ng_type & self::HIGHLIGHT_ID) {
-	$date_id = preg_replace("((ID:))", "<span style=\"{$live_highlight_style}\">$1", $date_id ."</span>");
+	$date_id = preg_replace("((ID:))", "<span class=\"live_highlight\" style=\"{$live_highlight_style}\">$1", $date_id ."</span>");
 }
 
 ?>
